29f1c6fc1ac5f708d31897e5f4b5931a10bb4ec0
Benoit Jadinon <j@dinonbeno.it>
2020-03-24 09:17:21 +0100

    fix example

--- example/lib/params.dart
+++ example/lib/params.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_zendesk_support/flutter_zendesk_support.dart';
 
-var zendeskSupportSettings = ZendeskSupportSettings(
+var zendeskSupportSettings = SupportSettings(
   appId: "_CHANGE_ME_",
   clientId: "_CHANGE_ME_",
   url: "_CHANGE_ME_",
